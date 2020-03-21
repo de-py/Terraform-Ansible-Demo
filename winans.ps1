53fd2ff32be33c7bd04307e89d1cd353cdd6258d
Dalton Ellis <dalton.ellis@trojans.dsu.edu>
2020-03-21 17:15:41 -0500

    updating windows install script

---
+++ winans.ps1
@@ -0,0 +1,10 @@
+#Sets up listenrs (http, https) with self-signed certificate w/ Basic auth.
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+
+
