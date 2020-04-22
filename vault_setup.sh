2f498c9d9655644a70be8562c74f5f8a499f0350
Dalton Ellis <dalton.ellis@trojans.dsu.edu>
2020-04-21 23:43:55 -0500

    uploading vault setup script

---
+++ vault_setup.sh
@@ -0,0 +1,22 @@
+# Download vault binary
+curl -o vault.zip https://releases.hashicorp.com/vault/1.4.0/vault_1.4.0_linux_amd64.zip
+
+# Install Unzip and unzip vault
+sudo yum install -y unzip
+unzip vault.zip
+
+# Move vault to /usr/local/bin
+sudo mv vault /usr/local/bin
+
+# Make this available to path. May not be needed?
+PATH=${PATH}:/usr/local/bin
+
+
+# Install auto complete commands
+vault -autocomplete-install
+
+#run dev in tmux with
+#vault server -dev
+
+
+
